f3159dec4ba8e3e084cc3b4fba8c7945d5883f6a
Fahad Iqbal <bigdogempire4@gmail.com>
2025-12-02 20:49:33 +0000

    why did ou chage the colour of the icon

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils';
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn('flex items-center gap-2', className)}>
-      <MessagesSquare className="h-16 w-16 md:h-8 md:w-8" />
+      <MessagesSquare className="h-12 w-12 md:h-8 md:w-8" />
       <h1 className="text-5xl font-bold tracking-tight md:text-2xl">
         <span className="text-primary">Chrome</span>
         <span className="text-foreground">Chat</span>
