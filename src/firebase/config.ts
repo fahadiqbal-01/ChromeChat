87c0c896ed41d05a1117c254798c9f28edf81e5d
Fahad Iqbal <bigdogempire4@gmail.com>
2025-12-02 16:31:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-49475671-65bd2",
+  "appId": "1:97021097956:web:552dd72d7dfad79ecdb9f1",
+  "apiKey": "AIzaSyBU5P-_ElYgJvVSeqg8f8HbinEAGLF0mvA",
+  "authDomain": "studio-49475671-65bd2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "97021097956"
+};
