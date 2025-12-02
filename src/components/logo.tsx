910fe8a2ede380fb44e768969b11af268fa33949
Fahad Iqbal <bigdogempire4@gmail.com>
2025-12-02 20:47:09 +0000

    increase the size of the  marked website logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,8 +4,8 @@ import { cn } from '@/lib/utils';
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn('flex items-center gap-2', className)}>
-      <MessagesSquare className="h-16 w-16 text-primary md:h-8 md:w-8" />
-      <h1 className="text-6xl font-bold tracking-tight md:text-3xl">
+      <MessagesSquare className="h-16 w-16 text-primary md:h-10 md:w-10" />
+      <h1 className="text-6xl font-bold tracking-tight md:text-4xl">
         <span className="text-primary">Chrome</span>
         <span className="text-foreground">Chat</span>
       </h1>
