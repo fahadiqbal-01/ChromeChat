150ee302f61b6d4005ee2af4d4fd77d0acbf63c0
Fahad Iqbal <bigdogempire4@gmail.com>
2025-12-02 20:48:56 +0000

    you didn't increase it for small devices

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,8 +4,8 @@ import { cn } from '@/lib/utils';
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn('flex items-center gap-2', className)}>
-      <MessagesSquare className="h-16 w-16 text-primary md:h-8 md:w-8" />
-      <h1 className="text-6xl font-bold tracking-tight md:text-2xl">
+      <MessagesSquare className="h-16 w-16 md:h-8 md:w-8" />
+      <h1 className="text-5xl font-bold tracking-tight md:text-2xl">
         <span className="text-primary">Chrome</span>
         <span className="text-foreground">Chat</span>
       </h1>
