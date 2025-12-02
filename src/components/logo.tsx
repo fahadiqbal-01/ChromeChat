c7fef9ea4381e0e1e8b67a532131d07cec3b9095
Fahad Iqbal <bigdogempire4@gmail.com>
2025-12-02 20:53:31 +0000

    maintain reponsiveness for all devices wtf is this

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,8 +4,8 @@ import { cn } from '@/lib/utils';
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn('flex items-center gap-2', className)}>
-      <MessagesSquare className="h-16 w-16 md:h-8 md:w-8" />
-      <h1 className="text-7xl font-bold tracking-tight md:text-2xl">
+      <MessagesSquare className="h-10 w-10 md:h-8 md:w-8" />
+      <h1 className="text-4xl font-bold tracking-tight md:text-2xl">
         <span className="text-primary">Chrome</span>
         <span className="text-foreground">Chat</span>
       </h1>
