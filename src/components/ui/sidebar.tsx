8f2144258ab8c997141a81b83dbcefdbfd355aa7
Fahad Iqbal <bigdogempire4@gmail.com>
2025-12-02 20:27:57 +0000

    now dubble the side of toogle button for mobile phones

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -271,7 +271,7 @@ const SidebarTrigger = React.forwardRef<
       data-sidebar="trigger"
       variant="ghost"
       size="icon"
-      className={cn("h-7 w-7", className)}
+      className={cn("h-7 w-7 md:h-14 md:w-14", className)}
       onClick={(event) => {
         onClick?.(event)
         toggleSidebar()
